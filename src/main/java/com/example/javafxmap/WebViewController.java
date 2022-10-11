f59d6accdd17657025064cb2afacd9456b81ea82
jinuya <111329324+jinuya@users.noreply.github.com>
2022-10-11 12:58:46 +0900

    1011

--- src/main/java/com/example/javafxmap/WebViewController.java
+++ src/main/java/com/example/javafxmap/WebViewController.java
@@ -10,10 +10,10 @@ public class WebViewController {
     private WebView webView;
 
     private WebEngine browser;
-    
+
     @FXML
     private void initialize() {
         browser = webView.getEngine();
-        browser.load("https://dangdangmap.net/pet/tour/map");
+        browser.load("http://localhost:63342/javaFXMap/com/example/javafxmap/index.html?_ijt=v1jpl8kmado64cq4r96dbds7s7&_ij_reload=RELOAD_ON_SAVE");
     }
 }
